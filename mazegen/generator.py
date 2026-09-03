cfd6a6fcb911d2a4d07b21014b32e96915622645
lbalderr <lbalderr@student.42sp.org.br>
2026-09-03 11:56:51 -0300

    Generate initial class structure

--- mazegen/generator.py
+++ mazegen/generator.py
@@ -1,2 +1,64 @@
+from mazegen.grid import Grid
+
+
 class MazeGenerator():
-    ...
+    """
+    """
+
+    def __init__(
+        self, width: int, height: int, is_perfect: bool,
+        entry_coords: tuple[int, int], exit_coords: tuple[int, int]
+    ) -> None:
+        """
+        """
+        self.grid = Grid(width, height)
+        self.is_perfect: bool = is_perfect
+        self.entry: tuple[int, int] = entry_coords
+        self.exit: tuple[int, int] = exit_coords
+
+    def generate(self) -> None:
+        """
+        """
+        # Public method that will call the other private methods
+        ...
+
+    def export(self) -> str:
+        """
+        """
+        # Translates the final shape of the maze into the hex required format
+        return ""
+
+    def render(self) -> None:
+        """
+        """
+        # Generates visual representation of the maze
+        ...
+
+    def _reserve_42_pattern(self) -> None:
+        """
+        """
+        # The pre-processing phase. Calculates the center of the grid, verifies
+        # the matrix is large enough, and forcibly sets visited = True on the
+        # specific cells forming the "42" shape. Because they are marked as
+        # visited before the core algorithm starts, the pathfinder will
+        # naturally weave around them without touching their walls.
+        ...
+
+    def _build_maze(self) -> None:
+        """
+        """
+        # Core generation algorithm
+        # Loops through the grid using Grid.get_unvisited_neighbors() to select
+        # paths and Grid.connect_cells() to securely break the walls
+        # bidirectionally.
+        ...
+
+    def _create_loops(self) -> None:
+        """
+        """
+        # The post-processing Pac-Man phase. If self.is_perfect is False, this
+        # method scans the grid for "dead ends" (cells with 3 closed walls).
+        # It selects a random adjacent, valid, non-boundary cell and
+        # utilizes Grid.connect_cells() to knock down a wall,
+        # establishing a cyclical path.
+        ...
