5f6b48e84eb987fe34f87197fdc2449ca8c63ead
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-09-03 20:36:14 -0300

    refact(maze_config): it adds docstrings into ConfigValidator

--- maze_config/config_validator.py
+++ maze_config/config_validator.py
@@ -3,19 +3,42 @@ import re
 
 
 class ConfigValidator:
-    def __init__(self, path: str) -> None:
-        self._path = path
+    """Validate the configuration file and its syntax."""
 
     @staticmethod
     def file_exists(path: str) -> bool:
+        """Check whether a file exists.
+
+        Args:
+            path: The path of the file to be checked.
+
+        Returns:
+            True if a file exists, False otherwise.
+        """
         return os.path.isfile(path)
 
     @staticmethod
     def is_readable(path: str) -> bool:
+        """Check whether a file is readable.
+
+        Args:
+            path: The path of the file to check permissions.
+
+        Returns:
+            True if file has reading permissions, False otherwise.
+        """
         return os.access(path, os.R_OK)
 
     @classmethod
     def is_valid_line(cls, line: str) -> bool:
+        """Check whether a given line has valid configuration syntax.
+
+        Args:
+            line: A string that represents a line in the configuration file.
+
+        Returns:
+            True if line has a valid syntax, False otherwise.
+        """
         stripped = line.strip()
         if len(stripped) == 0:
             return True
@@ -27,5 +50,13 @@ class ConfigValidator:
 
     @classmethod
     def is_valid_file_syntax(cls, path: str) -> bool:
+        """Check whether all lines have valid syntax.
+
+        Args:
+            path: The path of the file to have its lines checked.
+
+        Returns:
+            True if the syntax of all lines are valid, False otherwise.
+        """
         with open(path, "r") as config_file:
             return all(cls.is_valid_line(line) for line in config_file)
