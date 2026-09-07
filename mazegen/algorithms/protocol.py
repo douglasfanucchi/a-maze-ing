e2c6389e9c595809df6b2f23d6ac048dc3994a04
Douglas Fanucchi <douglasfanucchi@gmail.com>
2026-09-06 21:18:07 -0300

    refact(mazegen.algorithms.MazeAlgorithm): it adds seed on algo constructor

--- mazegen/algorithms/protocol.py
+++ mazegen/algorithms/protocol.py
@@ -1,9 +1,18 @@
 from typing import Protocol
 from mazegen.grid import Grid
+from typing import Optional
 
 
 class MazeAlgorithm(Protocol):
     """Defines the strict interface for maze generation algorithms."""
+
+    def __init__(self, seed: Optional[int]):
+        """Initiate the algorithm default state.
+        
+        Args:
+            seed: Seed to reproduce a specific paths creation.
+        """
+        ...
 
     def execute(self, grid: Grid) -> None:
         """Execute the spanning tree algorithm to carve paths.
