c50090c5b93b01eba97941a8c1851e9d23549077
lbalderr <lbalderr@student.42sp.org.br>
2026-09-08 19:01:53 -0300

    Print error message when argv < 2

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,4 +1,5 @@
 import sys
+import random
 from mazegen.grid import Grid
 from mazegen.generator import MazeGenerator
 from mazegen.algorithms import DFS
@@ -8,16 +9,19 @@ from mazegen.algorithms import MazeAlgorithm
 
 
 def main() -> None:
-    config: Config
+    if len(sys.argv) < 2:
+        sys.stderr.write("Error: Too few arguments\n")
+        sys.exit(1)
     try:
         config = Config(sys.argv[1])
     except (PermissionError, FileNotFoundError, ValueError) as e:
         sys.stderr.write(f"Error: {e}\n")
         sys.exit(1)
 
+    random.seed(config.get("SEED"))
     grid = Grid(config.get("WIDTH"), config.get("HEIGHT"))
     algorithms: dict[str, MazeAlgorithm] = {
-        "DFS": DFS(config.get("SEED")),
+        "DFS": DFS(),
     }
     algorithm: str = config.get("ALGORITHM")
     try:
@@ -29,15 +33,10 @@ def main() -> None:
             exit_coords=config.get("EXIT")
         )
         generator.generate()
-        if not generator.forty_two:
-            print(
-                "WARNING! The maze does not support 42 pattern.",
-                file=sys.stderr
-            )
         solver = Solver(grid, config.get("ENTRY"), config.get("EXIT"))
         with open(config.get("OUTPUT_FILE"), "w") as output_file:
             print(generator.export(), file=output_file)
-            print(solver.shortest_paths()[0], file=output_file)
+            print(solver.shortest_paths[0], file=output_file)
     except ValueError as e:
         sys.stderr.write(f"Error: {e}\n")
         sys.exit(1)
