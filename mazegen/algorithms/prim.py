c2f3afa6123396e88658765b466ddd50c231134d
Lucas Paiva <lpaiva234@gmail.com>
2026-09-14 15:57:29 -0300

    Create file for Prim algorithm

---
+++ mazegen/algorithms/prim.py
@@ -0,0 +1,13 @@
+import random
+from ..grid import Grid
+from ..cell import Cell
+from ..direction import Direction
+from .protocol import MazeAlgorithm
+
+
+class Prim(MazeAlgorithm):
+    """Implements a randomized Prim's maze generation strategy."""
+    def execute(self, grid: Grid) -> None:
+        """
+        """
+        ...
