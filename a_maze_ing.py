7943ad105010f082c65b82a3a25a63a32ddb7083
lbalderr <lbalderr@student.42sp.org.br>
2026-09-17 17:51:17 -0300

    Add visualization for maze

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -9,6 +9,8 @@ from mazegen.algorithms import Prim
 from solver import Solver
 from maze_config import Config
 from mazegen.algorithms import MazeAlgorithm
+from mlx import Mlx
+from mlx_api import Image, WallsImage
 
 
 def main() -> None:
@@ -55,6 +57,54 @@ def main() -> None:
         with open(config.get("OUTPUT_FILE"), "w") as output_file:
             print(generator.export(), file=output_file)
             print(solver.shortest_path, file=output_file)
+        mlx = Mlx()
+        conn = mlx.mlx_init()
+        dimensions = mlx.mlx_get_screen_size(conn)
+        dimension = min(int(dimensions[1] * 0.8), int(dimensions[2] * 0.8))
+        ratio = config.get("WIDTH") / config.get("HEIGHT")
+        width = min(int(ratio * dimension), dimension)
+        height = int(width / ratio)
+        win_ptr = mlx.mlx_new_window(
+            conn, width, height, "The Friendly Neighborhood Devs"
+        )
+        color1 = (255, 43, 55, 132)
+        color2 = (255, 177, 19, 19)
+
+        def test(keycode, param):
+            print(keycode)
+            if keycode == 65307:
+                mlx.mlx_loop_exit(conn)
+            print(param)
+        mlx.mlx_key_hook(win_ptr, test, None)
+        walls_image = WallsImage(
+            mlx,
+            conn,
+            generator,
+            width,
+            height - 20,
+            10,
+            color1,
+            color2,
+        )
+        background = Image(mlx, conn, width, height)
+        for i in range(0, width):
+            for j in range(0, height):
+                background.put_pixel(i, j, color1)
+        white_space = width - walls_image._wall_total * walls_image._maze.grid.width
+        margin_left = white_space // 2
+        background.render_on_window(win_ptr, 0, 0)
+        wall_frames = walls_image.render_frames(win_ptr, (margin_left, 0))
+
+        def frames(_):
+            next(wall_frames)
+        mlx.mlx_loop_hook(conn, frames, None)
+
+        def close(mlx: Mlx):
+            mlx.mlx_loop_exit(conn)
+        mlx.mlx_hook(win_ptr, 33, 0, close, mlx)
+        mlx.mlx_loop(conn)
+        walls_image.destroy()
+        mlx.mlx_release(conn)
     except ValueError as e:
         sys.stderr.write(f"Error: {e}\n")
         sys.exit(1)
